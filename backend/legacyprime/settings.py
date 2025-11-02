96117b53c304f36b1bce0a9b4b4eec65cd032626
Giddyjr7 <menegideon84@gmail.com>
2025-11-02 20:18:56 +0100

    change email port and siwitch from TLS to SSL

--- backend/legacyprime/settings.py
+++ backend/legacyprime/settings.py
@@ -239,11 +239,11 @@ SESSION_SAVE_EVERY_REQUEST = True
 # Email (SMTP) - pull from env
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'no-reply@legacyprime.com')
 EMAIL_TIMEOUT = 15
 SMTP_DEBUG_LEVEL = 1
