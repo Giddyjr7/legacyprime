06847cd14d10ff92c550a197aa3a8d9c11750f10
Giddyjr7 <menegideon84@gmail.com>
2025-11-07 16:11:55 +0100

    Nov-7-2025 | (4:11pm) Updated api.ts to automatically switch between environment

--- backend/legacyprime/settings.py
+++ backend/legacyprime/settings.py
@@ -73,7 +73,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'legacyprime.middleware.DebugMiddleware',
+    # 'legacyprime.middleware.DebugMiddleware',
     'legacyprime.jwt_debug.JWTDebugMiddleware',
 ]
 
