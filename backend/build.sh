149f9726599df1f49e0ef93e72803a04614f5c2b
Giddyjr7 <menegideon84@gmail.com>
2025-11-12 07:34:54 +0100

    Nov-11-2025 | (7:34am) Updated Render.yaml File for deployment

--- backend/build.sh
+++ backend/build.sh
@@ -11,3 +11,6 @@ python manage.py migrate
 
 # Collect static files (needed if you serve static files like Django Admin)
 python manage.py collectstatic --no-input
+
+
+python manage.py createsuperuser
